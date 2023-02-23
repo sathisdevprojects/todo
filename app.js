dbb1f160c96d459ce965af5e8d93cc1d671c04c0
Sathishkumar V <121035747+sathisdevprojects@users.noreply.github.com>
2023-02-23 13:39:33 +0530

    update and second commit

--- app.js
+++ app.js
@@ -126,6 +126,6 @@ app.get("/:post", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server strated on port 3000");
 });
